62e5be32a91d262f5ef9953eb0ba9e79c4fcade8
Patrick Olusaba <patrick.olusaba@gmail.com>
2026-01-23 11:31:37 +0300

    hotel.ts

--- src/data/hotels.ts
+++ src/data/hotels.ts
@@ -1,9 +1,3 @@
-// src/data/hotels.ts
-
-import giraffeManor from "../assets/images/hotel/giraffemanor.jpg";
-import hemingways from "../assets/images/hotel/hemingways.jpg";
-import norfolk from "../assets/images/hotel/norfolk.jpg";
-
 export interface Hotel {
     id: number;
     name: string;
@@ -16,60 +10,60 @@ export const hotels: Hotel[] = [
         id: 1,
         name: "Giraffe Manor",
         location: "Karen, Nairobi",
-        image: giraffeManor
+        image: "/images/hotel/giraffemanor.jpg"
     },
     {
         id: 2,
-        name: "Hemingways Nairobi",
+        name: "Hemingway's Nairobi",
         location: "Westlands, Nairobi",
-        image: hemingways
+        image: "/images/hotel/hemingways.jpg"
     },
     {
         id: 3,
         name: "Fairmont The Norfolk",
         location: "CBD, Nairobi",
-        image: norfolk
+        image: "/images/hotel/norfolk.jpg"
     },
     {
         id: 5,
         name: "Giraffe Manor",
         location: "Karen, Nairobi",
-        image: giraffeManor
+        image: "/images/hotel/giraffemanor.jpg"
     },
     {
         id: 6,
-        name: "Hemingways Nairobi",
+        name: "Hemingway's Nairobi",
         location: "Westlands, Nairobi",
-        image: hemingways
+        image: "/images/hotel/hemingways.jpg"
     },
     {
         id: 7,
         name: "Fairmont The Norfolk",
         location: "CBD, Nairobi",
-        image: norfolk
+        image: "/images/hotel/norfolk.jpg"
     },
     {
         id: 8,
         name: "Giraffe Manor",
         location: "Karen, Nairobi",
-        image: giraffeManor
+        image: "/images/hotel/giraffemanor.jpg"
     },
     {
         id: 9,
-        name: "Hemingways Nairobi",
+        name: "Hemingway's Nairobi",
         location: "Westlands, Nairobi",
-        image: hemingways
+        image: "/images/hotel/hemingways.jpg"
     },
     {
         id: 10,
         name: "Fairmont The Norfolk",
         location: "CBD, Nairobi",
-        image: norfolk
+        image: "/images/hotel/norfolk.jpg"
     },
     {
         id: 11,
         name: "Fairmont The Norfolk",
         location: "CBD, Nairobi",
-        image: norfolk
+        image: "/images/hotel/norfolk.jpg"
     }
 ];
